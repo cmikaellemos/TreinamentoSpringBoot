738c399aba0cc7672672c86c5507eef2a7686259
Mikael Lemos <mikaellemos@alu.ufc.br>
2017-07-23 23:23:48 -0300

    update back-end

--- demo/src/main/java/br/ufc/demo/pubs/PubsRepository.java
+++
@@ -1,8 +0,0 @@
-package br.ufc.demo.pubs;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource(collectionResourceRel = "pubs", path = "pubs")
-public interface PubsRepository extends JpaRepository<Pubs, Integer> {
-}
